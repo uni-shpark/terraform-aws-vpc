8271081e384c7409cdc89784b808ed786fbadfef
root <root@image-registry.openshift-image-registry>
2021-01-31 19:37:58 -0500

    label location modify

--- main.tf
+++ main.tf
@@ -3,7 +3,8 @@
 ########
 
 module "label" {
-  source     = "../terraform-aws-label"
+  source  = "app.terraform.io/uni-shpark/label/aws"
+  version = "1.0.0"
   namespace  = var.namespace
   name       = var.name
   stage      = var.stage
